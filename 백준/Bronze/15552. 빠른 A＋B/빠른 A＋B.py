a3fece1ffc5cae5501afec92acb038edc385cbed
HaSeong Jung <107913513+HaseongJung@users.noreply.github.com>
2023-09-22 21:56:57 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1188 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,13 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+def main():
+    T = int(input())
+
+    for _ in range(T):
+        A, B = map(int, input().split())
+        print(A + B)
+
+if __name__ == "__main__":
+    main()
