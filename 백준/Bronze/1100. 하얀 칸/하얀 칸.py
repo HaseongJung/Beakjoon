23e803e9d685a392a236df9972ee8056acd89511
HaSeong Jung <107913513+HaseongJung@users.noreply.github.com>
2023-10-15 22:00:53 +0900

    [Bronze II] Title: 하얀 칸, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,15 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+chess = []
+for _ in range(8):
+    chess.append(list(input()))
+
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if (i+j)%2 == 0 and chess[i][j] == 'F':
+            cnt += 1
+            
+print(cnt)
